40d5d94bbea2a314f706c20a578dbce31d732c69
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2023-07-21 12:54:23 +0100

    Allow using another username for the code owner (#78)
    So that for example node images can use the already defined node user already uid 1000
    
    The assumption is the code owner has a /home/{code owner} home directory, but it can be overridden

--- docker/image/console/root/usr/lib/task/skeleton/apply.sh
+++ docker/image/console/root/usr/lib/task/skeleton/apply.sh
@@ -2,5 +2,8 @@
 
 function task_skeleton_apply()
 {
-    run rsync --exclude='*.twig' --exclude='_twig' -a /home/build/application/skeleton/ /app/
+    local CODE_OWNER_HOME
+    CODE_OWNER_HOME="$(getent passwd "${CODE_OWNER}" | cut -d: -f6)"
+
+    run rsync --exclude='*.twig' --exclude='_twig' -a "${CODE_OWNER_HOME}"/application/skeleton/ /app/
 }
