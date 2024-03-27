d25d1d52a8f47cc4f67802224537e91e29cadf63
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2024-03-27 09:28:51 +0000

    quote printed mount line entry

--- docker/image/console/root/entrypoint.dynamic.sh
+++ docker/image/console/root/entrypoint.dynamic.sh
@@ -41,7 +41,7 @@ resolve_volume_mount_strategy()
         elif (mount | grep "/app type btrfs") > /dev/null 2>&1; then
             STRATEGY="host-linux-normal"
         elif (mount | grep "/app type") > /dev/null; then
-            echo "error: unsupported mount type for $(mount | grep "/app type")" >&2
+            echo "error: unsupported mount type for '$(mount | grep "/app type")'" >&2
             exit 1
         else
             echo "error: mount for /app not found" >&2
